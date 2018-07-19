a8d34d6a4dbee3984c19e0b35a811b0a83326507
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2018-07-19 15:57:03 +0300

    Simple test

--- source/components/ProfileHeader/index.js
+++ source/components/ProfileHeader/index.js
@@ -9,7 +9,7 @@ export default class ProfileHeader extends React.Component {
         return (
             <div className = 'header'>
                 <div>
-                    <h1>Илон Маск</h1>
+                    <h1>Илон Musk</h1>
                     <span>
                         Предприниматель, изобретатель, инженер, инвестор
                     </span>
